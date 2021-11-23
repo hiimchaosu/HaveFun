ee236dbb71980e7d527efa2ee9ee254a2d9a490b
hiimChaosu <patryksob222@gmail.com>
2021-11-23 20:17:20 +0100

    Added new event handler for @everyone

--- bot.py
+++ bot.py
@@ -1,10 +1,17 @@
 import discord
 from discord.ext import commands
 
-client = commands.Bot(command_prefix = '.')
+client = discord.Client()
 
 @client.event
 async def on_ready():
-    print("Bot online.");
+    print('{0.user} online.'.format(client));
 
-client.run('TOKEN HERE');
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return;
+    if "@everyone" in message.content:
+        await message.channel.send('<:ping:768891917848281119> {0}'.format(message.author.mention));
+
+client.run('OTEyNjQ1MTQxMjU5NTEzODc2.YZy9BQ.DbSPxgYxI_2MbPs2OjLE6rSLCxM');
