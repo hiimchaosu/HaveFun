1697b7fe91bb486a87c40ad077741ef9f8a31faa
hiimChaosu <patryksob222@gmail.com>
2021-11-23 16:09:19 +0100

    Token needs to be erased for safety purposes.

--- bot.py
+++ bot.py
@@ -7,4 +7,4 @@ client = commands.Bot(command_prefix = '.')
 async def on_ready():
     print("Bot online.");
 
-client.run('OTEyNjQ1MTQxMjU5NTEzODc2.YZy9BQ.9vIJ0gI9kZHaXfiJLqqn2Zmgb-M');
+client.run('TOKEN HERE');
