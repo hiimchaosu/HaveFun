44bac5706e97995012cd744314e710f137da995d
Patryk <patryksob222@gmail.com>
2021-11-24 15:27:30 +0100

    Added a handler for when someone's typing a message (fun)

--- bot.py
+++ bot.py
@@ -9,9 +9,10 @@ handler = logging.FileHandler(filename='discord.log', encoding='utf-8', mode='w'
 handler.setFormatter(logging.Formatter('%(asctime)s:%(levelname)s:%(name)s: %(message)s'))
 logger.addHandler(handler)
 
+intents = discord.Intents.all();
 client = discord.Client()
 
-def maturka_time(): #Counting days inbetwen two dates.
+def maturka_time(): # Counting days inbetwen two dates.
     current_date = datetime.date.today()
     target_date = datetime.date(2022, 5, 4)
     days_diff = target_date - current_date
@@ -21,7 +22,7 @@ def maturka_time(): #Counting days inbetwen two dates.
 async def on_ready():
     print('{0.user} online and operating.'.format(client));
 
-@client.event
+@client.event # shenanigans - Chaosu
 async def on_message(message):
     if message.author == client.user:
         return;
@@ -30,4 +31,10 @@ async def on_message(message):
     if "matura" in message.content:
         await message.channel.send(
             'Matury zaczynają się 4 Maja 2022, środa o godz. 9.00. Szykuj dupe <@!697518297096257577>, bo zostało ci ', maturka_time())
+
+@client.event # Ping user when he's typing anything - Chaosu
+async def on_typing(channel, user, when):
+    if user.id == 222439577284116480 and channel.id == 912763830969454602:
+        await channel.send('Widze jak tam sobie piszesz {0}'.format(user.mention));
+        print(f"{user} is typing message in {channel} {when}"); # Additional comment for console output
 client.run(HFToken());
