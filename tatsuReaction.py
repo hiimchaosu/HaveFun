e0e1a33a14d91edc6d9810c2726a1c03fd173d64
hiimChaosu <patryksob222@gmail.com>
2021-12-03 20:48:09 +0100

    tatsuReaction fixed, added small "Widzisz mnie?" for a bot to respond to

--- tatsuReaction.py
+++ tatsuReaction.py
@@ -9,17 +9,18 @@ from discord.ext import commands
 class tatsuReaction(commands.Cog):
     def __init__(self, client):
         self.client = client
-
-    bot = commands.Bot(command_prefix='$', intents=discord.Intents.all())
+        self.bot = commands.Bot(command_prefix='$', intents=discord.Intents.all())
     @commands.Cog.listener()  # Somehow works, tho throws a small error, to fix later.
     async def on_message(self, message):
-        print(f"{message.channel}: {message.author}: {message.content}")
         if message.author == self.bot.user:
             return
-        if "test" in message.content:
-            print("Testujesz cos?")
-        await self.bot.process_commands(message)
-
+        if message.author.id == 172002275412279296 and "has leveled up!" in message.content:
+            await message.channel.send("{0} jak zwykle z rigczem.".format(message.author.mention))
+        if "Widzisz mnie?" in message.content:
+            with open("brek.jpeg",'rb') as f:
+                picture = discord.File(f)
+            await message.channel.send("Widze {0}".format(message.author.mention))
+            await message.channel.send(file=picture)
 
 def setup(client):
     client.add_cog(tatsuReaction(client))
