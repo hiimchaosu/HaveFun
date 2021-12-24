1d07f35045bd19f2c0b61a94c74fd68a9a644989
hiimChaosu <patryksob222@gmail.com>
2021-12-24 19:20:36 +0100

    custom commands thrown into a separate file 'botCommands.py'

--- bot.py
+++ bot.py
@@ -1,23 +1,17 @@
 import discord
-import datetime
 from discord.ext import commands
 from HaveFunToken import HFToken
+# Importing cogs for the bot
+import botCommands
 import music
 import discordLogging
 import tatsuReaction
 
-bot = commands.Bot(command_prefix='$', intents = discord.Intents.all(), case_insensitive=True)
+bot = commands.Bot(command_prefix='$', intents = discord.Intents.all(), case_insensitive=True, help_command=None)
 
-cogs = [music, discordLogging, tatsuReaction]
+cogs = [music, discordLogging, tatsuReaction, botCommands]
 for i in range(len(cogs)):
     cogs[i].setup(bot)
-
-# Counting days inbetween two dates.
-def maturka_time():
-    current_date = datetime.date.today()
-    target_date = datetime.date(2022, 5, 4)
-    days_left = target_date - current_date
-    return days_left.days
 
 # wait for bot's responsiveness and set it's status
 @bot.event
@@ -30,23 +24,9 @@ async def on_message(message):
     print(f"{message.channel}: {message.author}: {message.content}")
     if message.author == bot.user:
         return
-    if "matura" in message.content:
-        await message.channel.send(
-            f'Matury zaczynają się 4 Maja 2022, środa o godz. 9.00. Szykuj dupe <@!697518297096257577>, bo zostało ci {maturka_time()} dni. ')
     if "@everyone" in message.content:
         await message.channel.send('<:ping:768891917848281119> {0}'.format(message.author.mention))
     await bot.process_commands(message)
-
-@bot.command()
-async def R6(channel):
-    emote = "<:kitkuPaf:848926844832186388>"
-    await channel.send(f"Its time for a game of R6 honey: <@!404579956874674176> {emote} <@!230697738784735232> {emote} <@!532665219927769099> {emote} <@!697518297096257577> {emote} <@!300652757000519680> {emote} <@!378627693606076444>")
-    message_R6 = [await channel.send("17.30-18.30"),
-                await channel.send("18.30-19.30"),
-                await channel.send("19.30-20.30")]
-    #embed = discord.Embed(title='Testy', color=0x00ff00) - This lil guy makes a fancy little box. Saved to play with later
-    for i in message_R6:
-        await i.add_reaction("<:GHOk:793607140735451156>")
 
 #@bot.event
 #async def on_raw_reaction_add(payload):
