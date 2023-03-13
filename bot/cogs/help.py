2979f1d3867aaf11da492d37aff7325b26a9fb74
Patryk <patryksob222@gmail.com>
2023-03-13 17:37:06 +0100

    help function now dynamically shows the prefix instead of being hardcoded

--- bot/cogs/help.py
+++ bot/cogs/help.py
@@ -1,6 +1,7 @@
 import discord
 from discord.ext import commands
 
+from bot.bot import PREFIX
 
 class help(commands.Cog):
     def __init__(self, bot):
@@ -14,17 +15,17 @@ class help(commands.Cog):
             colour=ctx.author.colour)
         embed.set_author(name="hiimChaosu", icon_url="https://avatars.githubusercontent.com/u/25712415?v=4",
                          url="https://github.com/hiimchaosu")
-        embed.add_field(name="connect", value="[$connect (channel_name)] - connect to a channel", inline=False)
+        embed.add_field(name="connect", value=f"[{PREFIX}connect (channel_name)] - connect to a channel", inline=False)
         embed.add_field(name="play/p",
-                        value="[$play/$p (SONG_URL / SONG_NAME)] - play a song of your choice, or use to resume playback",
+                        value=f"[{PREFIX}play/{PREFIX}p (SONG_URL / SONG_NAME)] - play a song of your choice, or use to resume playback",
                         inline=False)
-        embed.add_field(name="disconnect", value="[$disconnect] - disconnect from a channel", inline=False)
-        embed.add_field(name="queue/q", value="[$queue/$q] - shows the queue for the next tracks if present",
+        embed.add_field(name="disconnect", value=f"[{PREFIX}disconnect] - disconnect from a channel", inline=False)
+        embed.add_field(name="queue/q", value=f"[{PREFIX}queue/{PREFIX}q] - shows the queue for the next tracks if present",
                         inline=False)
-        embed.add_field(name="pause", value="[$pause] - pauses the playback - to resume use $play", inline=False)
-        embed.add_field(name="stop", value="[$stop] - stops the playback", inline=False)
-        embed.add_field(name="next/skip", value="[$next/$skip] - goes to the next song", inline=False)
-        embed.add_field(name="previous", value="[$previous] - goes back to the previously played song", inline=False)
+        embed.add_field(name="pause", value=f"[{PREFIX}pause] - pauses the playback - to resume use $play", inline=False)
+        embed.add_field(name="stop", value=f"[{PREFIX}stop] - stops the playback", inline=False)
+        embed.add_field(name="next/skip", value=f"[{PREFIX}next/{PREFIX}skip] - goes to the next song", inline=False)
+        embed.add_field(name="previous", value=f"[{PREFIX}previous] - goes back to the previously played song", inline=False)
         embed.set_footer(
             text="Future changes in progress - if you have any ideas contact hiimChaosu#1703 or Kakari#3103")
         await ctx.send(embed=embed)
@@ -37,6 +38,7 @@ class help(commands.Cog):
                         value="Want to play a game? R6 - I'll ping people with this rank and give you times!",
                         inline=False)
         embed.add_field(name="matura", value="Command to remind our friend about her matura exams :)", inline=False)
+        embed.add_field(name="teczka", value="Check the archives of HF", inline=False)
         embed.add_field(name="essa", value="How much of a chill person are You?", inline=False)
         embed.add_field(name="rasista", value="How much racism is in You? Check it with this command!", inline=False)
         embed.add_field(name="tlen", value="Check your oxygen reserves", inline=False)
