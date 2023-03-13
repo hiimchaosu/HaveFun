2291dcaa180e4ba4defffbe8ea961652d01bdd24
Patryk <patryksob222@gmail.com>
2023-03-13 17:36:41 +0100

    functions for RNG parts of the cog rewritten

--- bot/cogs/botCommands.py
+++ bot/cogs/botCommands.py
@@ -3,10 +3,24 @@ from discord.ext import commands
 import datetime
 import random
 
-
 class botCommands(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
+
+    def rngLevel(self):
+        RNGCreate = random.randint(0,100)
+        RNGOption = [0,1,2,3,4]
+        print(RNGOption[0])
+        if RNGCreate <= 20:
+            return RNGOption[0], RNGCreate
+        elif 20 < RNGCreate < 60:
+            return RNGOption[1], RNGCreate
+        elif 60 <= RNGCreate <= 95:
+            return RNGOption[2], RNGCreate
+        elif 96 <= RNGCreate <= 99:
+            return RNGOption[3], RNGCreate
+        else:
+            return RNGOption[4], RNGCreate
 
     @commands.Cog.listener()
     async def on_message(self, ctx):
@@ -25,69 +39,44 @@ class botCommands(commands.Cog):
         await ctx.message.channel.send(
             f'Matury zaczynają się 4 Maja 2022, środa o godz. 9.00. Szykuj dupe <@!697518297096257577>, bo zostało ci {days_left.days} dni.')
 
-
     @commands.command(name="essa")
     async def essa(self, ctx):
-        poziom_essy = random.randint(0, 100)
-        wariat = format(ctx.message.author.mention)
-
-        if poziom_essy <= 20:
-            await ctx.message.channel.send(
-                f'Twój poziom essy wynosi: {poziom_essy}%, wariacie, słabo coś dzisiaj {wariat}')
-        elif poziom_essy > 20 and poziom_essy < 60:
-            await ctx.message.channel.send(
-                f'Twój poziom essy wynosi: {poziom_essy}%, lecimy nie śpimy, {wariat} :call_me:')
-        elif poziom_essy >= 60 and poziom_essy <= 95:
-            await ctx.message.channel.send(
-                f'Nooo, to jest dopiero chill, {poziom_essy}% {wariat} tak trzymaj! :call_me:')
-        elif poziom_essy >= 96 and poziom_essy <= 99:
-            await ctx.message.channel.send(
-                f'Gibon był palony? {wariat}, aż potężne {poziom_essy}% :call_me:')
-        elif poziom_essy == 100:
-            await ctx.message.channel.send(
-                f'Jaki {wariat} jest wychillowanyyyyy~ {poziom_essy}% :call_me: :call_me: :call_me:')
+        OPTION, PERCENTAGE = self.rngLevel()
+        Author = format(ctx.message.author.mention)
+        ELevel = [
+            f"Twój poziom essy wynosi: {PERCENTAGE}%, wariacie, słabo coś dzisiaj {Author}",
+            f"Twój poziom essy wynosi: {PERCENTAGE}%, lecimy nie śpimy, {Author} :call_me:",
+            f"Nooo, to jest dopiero chill, {PERCENTAGE}% {Author} tak trzymaj! :call_me:",
+            f"Gibon był palony? {Author}, aż potężne {PERCENTAGE}% :call_me:",
+            f"Jaki {Author} jest wychillowanyyyyy~ {PERCENTAGE}% :call_me: :call_me: :call_me:",
+        ]
+        await ctx.send(ELevel[OPTION])
 
     @commands.command(name="rasista")
     async def rasista(self, ctx):
-        poziom_rasisty = random.randint(0, 100)
-        rasista = format(ctx.message.author.mention)
-
-        if poziom_rasisty <= 20:
-            await ctx.message.channel.send(
-                f'Racism cancelled {poziom_rasisty}% - try your luck next time {rasista} <:brugSad:1012490361257599067>')
-        elif poziom_rasisty > 20 and poziom_rasisty < 60:
-            await ctx.message.channel.send(
-                f'Członek {rasista} i jego poziom {poziom_rasisty}% wskazuje na lekki <:cmonBrug:1012489207559766146>')
-        elif poziom_rasisty >= 60 and poziom_rasisty <= 95:
-            await ctx.message.channel.send(
-                f'Piękny przykład naszego członka klubu, {rasista}, {poziom_rasisty}%! <:cmonBrug:1012489207559766146>')
-        elif poziom_rasisty >= 96 and poziom_rasisty <= 99:
-            await ctx.message.channel.send(
-                f'Rasowy rasista {rasista} to jest poziom! {poziom_rasisty}% <:cmonBrug:1012489207559766146>')
-        elif poziom_rasisty == 100:
-            await ctx.message.channel.send(
-                f'NOWY PRZYWÓDCA, NASZ {rasista} - {poziom_rasisty}% <:brugHappy:1012490995163729930>')
+        OPTION, PERCENTAGE = self.rngLevel()
+        Author = format(ctx.message.author.mention)
+        RLevel = [
+            f"Racism cancelled {PERCENTAGE}% - try your luck next time {Author} <:brugSad:1012490361257599067>",
+            f"Członek {Author} i jego poziom {PERCENTAGE}% wskazuje na lekki <:cmonBrug:1012489207559766146>",
+            f"Piękny przykład naszego członka klubu, {Author}, {PERCENTAGE}%! <:cmonBrug:1012489207559766146>",
+            f"Rasowy rasista {Author} to jest poziom! {PERCENTAGE}% <:cmonBrug:1012489207559766146>",
+            f"NOWY PRZYWÓDCA -> {Author} - {PERCENTAGE}% <:brugHappy:1012490995163729930>",
+        ]
+        await ctx.send(RLevel[OPTION])
 
     @commands.command(name="tlen")
     async def tlen(self, ctx):
-        poziom_tlenu = random.randint(0, 100)
-        oddychacz = format(ctx.message.author.mention)
-
-        if poziom_tlenu <= 30:
-            await ctx.message.channel.send(
-                f'ALE DUSZNO, TLEN: {poziom_tlenu}% Lepiej łap za butle {oddychacz} <:harambe:1012493211878576259>')
-        elif poziom_tlenu > 30 and poziom_tlenu < 60:
-            await ctx.message.channel.send(
-                f'Nie jest źle {oddychacz}, skromne {poziom_tlenu}%. Jest czym oddychać. <:harambe:1012493211878576259>')
-        elif poziom_tlenu >= 60 and poziom_tlenu <= 95:
-            await ctx.message.channel.send(
-                f'Weź się podziel {oddychacz}, aż {poziom_tlenu}%! <:harambe:1012493211878576259>')
-        elif poziom_tlenu >= 96 and poziom_tlenu <= 99:
-            await ctx.message.channel.send(
-                f'Tak to można <:harambe:1012493211878576259> pooddychać. {oddychacz} - {poziom_tlenu}% <:harambe:1012493211878576259>')
-        elif poziom_tlenu == 100:
-            await ctx.message.channel.send(
-                f'Tlen nie jest dla {oddychacz} problemem {poziom_tlenu}%! <:xdd:1012493213703082034>')
+        OPTION, PERCENTAGE = self.rngLevel()
+        Author = format(ctx.message.author.mention)
+        TLevel = [
+            f"ALE DUSZNO, TLEN: {PERCENTAGE}% Lepiej łap za butle {Author} <:harambe:1012493211878576259>",
+            f"Nie jest źle {Author}, skromne {PERCENTAGE}%. Jest czym oddychać. <:harambe:1012493211878576259>",
+            f"Weź się podziel {Author}, aż {PERCENTAGE}%! <:harambe:1012493211878576259>",
+            f"Tak to można <:harambe:1012493211878576259> pooddychać. {Author} - {PERCENTAGE}% <:harambe:1012493211878576259>",
+            f"Tlen nie jest dla {Author} problemem {PERCENTAGE}%! <:xdd:1012493213703082034>",
+        ]
+        await ctx.send(TLevel[OPTION])
 
     @commands.command(name="R6")
     async def R6(self, ctx):
@@ -100,6 +89,11 @@ class botCommands(commands.Cog):
                       await ctx.send("Pass aka <@!300652757000519680> mówiący jutro")]
         for i in message_R6:
             await i.add_reaction("<:GHOk:793607140735451156>")
+
+    # For future bug and function testing
+    @commands.command(name="check")
+    async def checkCommand(self, ctx):
+        pass
 
 
 def setup(bot):
