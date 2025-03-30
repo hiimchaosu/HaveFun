5e0cf9177f143257521a8d3fc7a3196b09e4f8ef
Patryk <patryksob222@gmail.com>
2025-03-30 16:10:18 +0200

    twitter_link_fixed added to config.json

--- bot/cogs/linkfixer.py
+++ bot/cogs/linkfixer.py
@@ -1,18 +1,17 @@
 import discord
+import json
 from discord.ext import commands
 
 #TODO - make it a little bit less hardcoded, ok?
-
-TWITTER = "https://twitter.com"
-
+config = json.loads(open("../data/config.json").read())
 class twitterFixer(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
     @commands.Cog.listener("on_message")
     async def on_message(self, ctx):
-        if TWITTER in ctx.content and ctx.author != self.bot.user:
-            await ctx.channel.send(ctx.content[:8] + "fx" + ctx.content[8:])
+        if config["twitter_link"] in ctx.content and ctx.author != self.bot.user:
+            await ctx.channel.send(config["twitter_link_fixed"] + ctx.content[13:] + " by " + str(ctx.author))
             await ctx.delete()
 
 async def setup(bot):
