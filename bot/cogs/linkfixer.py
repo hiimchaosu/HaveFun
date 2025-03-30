76d32935c512d8a1dfe7b4f53b767e53ab2ef7fe
hiimChaosu <patryksob222@gmail.com>
2025-03-30 13:59:37 +0200

    Putting the "TWITTER" variable under "config.json"

--- bot/cogs/linkfixer.py
+++ bot/cogs/linkfixer.py
@@ -1,17 +1,16 @@
 import discord
+import json
 from discord.ext import commands
 
 #TODO - make it a little bit less hardcoded, ok?
-
-TWITTER = "https://twitter.com"
-
+config = json.loads(open("../data/config.json").read())
 class twitterFixer(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
     @commands.Cog.listener("on_message")
     async def on_message(self, ctx):
-        if TWITTER in ctx.content and ctx.author != self.bot.user:
+        if config["twitter_link"] in ctx.content and ctx.author != self.bot.user:
             await ctx.channel.send(ctx.content[:8] + "fx" + ctx.content[8:])
             await ctx.delete()
 
