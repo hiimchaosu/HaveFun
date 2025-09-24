10c6dca6e59cc89bf4563b55a4a287e7c676d455
hiimChaosu <patryksob222@gmail.com>
2025-09-24 09:15:28 +0200

    link fixing added for instagram reels

--- bot/cogs/linkfixer.py
+++ bot/cogs/linkfixer.py
@@ -1,17 +1,37 @@
 import discord
 import json
+import re
 from discord.ext import commands
 
-config = json.loads(open("../data/config.json").read())
+config = json.loads(open("/app/data/config.json").read())
 class twitterFixer(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
     @commands.Cog.listener("on_message")
-    async def on_message(self, ctx):
-        if config["twitter_link"] in ctx.content and ctx.author != self.bot.user:
-            await ctx.channel.send(config["twitter_link_fixed"] + ctx.content[13:] + " by " + str(ctx.author))
-            await ctx.delete()
+    async def on_message(self, message: discord.Message):
+        if message.author.bot:
+            return
+
+        replacements = {
+            config["instagram_link"]: config["instagram_link_fixed"],
+            config["twitter_link"]: config["twitter_link_fixed"],
+            config["x_link"]: config["x_link_fixed"]
+        }
+        new_content = message.content
+        replaced = False
+
+        for old, new in replacements.items():
+            if old in new_content:
+                new_content = new_content.replace(old, new)
+                replaced = True
+
+        # remove junk before question mark
+        new_content = re.sub(r"(https?://www\.kkinstagram\.com/\S+?)\?.*", r"\1", new_content)
+
+        if replaced or new_content != message.content:
+            await message.delete()
+            await message.channel.send(f"{new_content} (by {message.author})")
 
 async def setup(bot):
     if bot.config["test-server-id"] == "":
