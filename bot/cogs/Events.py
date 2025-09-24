fdcdec7bf5344227395893fe67652b1a47d47a7b
hiimChaosu <patryksob222@gmail.com>
2025-09-24 09:14:52 +0200

    fix Events.py - do not ever change it like that

--- bot/cogs/Events.py
+++ bot/cogs/Events.py
@@ -8,10 +8,11 @@ class Events(commands.Cog):
 
     @commands.Cog.listener("on_message")
     async def brek(self, message):
-        message.content = message.content.lower().replace(' ', '')
-        if 'widziszmnie?' in message.content:
+        brek_find = message.content
+        brek_find.lower().replace(' ', '')
+        if 'widziszmnie?' in brek_find:
             await message.channel.send("Widzeee {0}".format(message.author.mention),
-                                       file=discord.File('../images/brek.jpeg'))
+                                       file=discord.File('/app/images/brek.jpeg'))
     async def kazdyJeden(self, message):
         if "@everyone" in message.content:
             await message.channel.send('<:ping:768891917848281119> {0}'.format(message.author.mention))
